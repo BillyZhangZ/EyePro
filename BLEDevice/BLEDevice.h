303b02ce9daae8147c2f0706e2673d7be3ca2bf3
张志阳 <zhangzhiyang@zhangzhiyangs-MacBook-Air.local>
2015-08-26 23:00:57 +0800

    1.添加蓝牙工程 --目前是扫描心率设备 --EyePro工程依赖蓝牙静态库工程，需要修改目录依赖关系，参考http://my.oschina.net/w11h22j33/blog/204337 2.添加EyePro工程 --后台语音提示，支持汉语、英语、法语、德语，使用系统语言提示 --能够调节屏幕亮度 --能够本地推送 --显示软件运行时间，间隔为5秒

--- BLEDevice/BLEDevice.h
+++ BLEDevice/BLEDevice.h
@@ -1,13 +1,28 @@
 //
-//  BLEDevice.h
-//  BLEDevice
+//  HeartRateDevice.h
+//  BTLE Transfer
 //
-//  Created by 张志阳 on 8/26/15.
-//  Copyright (c) 2015 张志阳. All rights reserved.
+//  Created by 张志阳 on 15/5/13.
+//  Copyright (c) 2015年 Apple. All rights reserved.
 //
+#import <CoreBluetooth/CoreBluetooth.h>
 
-#import <Foundation/Foundation.h>
+@protocol HeartRateDelegate
 
-@interface BLEDevice : NSObject
+@required
+-(void)didUpdateHeartRate:(unsigned short) heartRate;
+-(void)isheartRateAvailable: (BOOL)available;
+@end
+
+
+@interface HeartRateDevice:NSObject
+@property (nonatomic) BOOL simulator;
+/**
+ *Called after init to start scan/connect heart rate device,
+ *delegate should be responsible for implementing HeartRateDelegate protocol
+ *whenever heart rate is updated, tdidUpdateHeartRate will be exected
+ **/
+-(instancetype)init:(id)delegate;
 
 @end
+
