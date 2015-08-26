59817ae4338bfd6da7da57966307628d38882afc
张志阳 <zhangzhiyang@zhangzhiyangs-MacBook-Air.local>
2015-08-26 18:24:38 +0800

    Initial Commit

---
+++ BLEDevice/BLEDevice.h
@@ -0,0 +1,13 @@
+//
+//  BLEDevice.h
+//  BLEDevice
+//
+//  Created by 张志阳 on 8/26/15.
+//  Copyright (c) 2015 张志阳. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BLEDevice : NSObject
+
+@end
---
+++ BLEDevice/BLEDevice.m
@@ -0,0 +1,13 @@
+//
+//  BLEDevice.m
+//  BLEDevice
+//
+//  Created by 张志阳 on 8/26/15.
+//  Copyright (c) 2015 张志阳. All rights reserved.
+//
+
+#import "BLEDevice.h"
+
+@implementation BLEDevice
+
+@end
